0bcb03ce89b256031bf21ddb21094be1c97a3ea8
David Ruiz <jesuruga@gmail.com>
2024-10-03 11:44:03 -0600

    Agregar dependencia de MercadoPago y controlador de pagos

--- src/adapters/routes/index.ts
+++ src/adapters/routes/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import choferRoutes from './chofer.routes';
 import usuariosRoutes from './usuarios.routes';
 import unidadRoutes from './unidad.routes';
-
+import paympController from '../controllers/PaympController';
 
 const router = express.Router();
 
@@ -15,5 +15,7 @@ router.use(`${apiVersion}/choferes`, choferRoutes);
 router.use(`${apiVersion}/users`, usuariosRoutes);
 
 router.use(`${apiVersion}/unidades`, unidadRoutes);
+
+router.use(`${apiVersion}/paymp`, paympController);
 
 export default router;
