64af2ea4bc8f2248adec776f3d20c1bf0befc2a8
David Ruiz <jesuruga@gmail.com>
2024-11-30 22:52:10 -0600

    Eliminar configuración de redirección HTTP a HTTPS y servidores HTTPS en las infraestructuras de notificaciones, pagos y usuarios

--- src/users/infrastructure/index.ts
+++ src/users/infrastructure/index.ts
@@ -1,5 +1,3 @@
-import https from 'https';
-import fs from 'fs';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -10,8 +8,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORTU || '';
-const httpPort = 80; // Puerto HTTP para redirección
-const httpsPort = process.env.PORTU || 443; // Puerto HTTPS
+
 
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
@@ -19,31 +16,9 @@ const apiLimiter = rateLimit({
     message: 'Demasiadas peticiones desde esta IP, por favor intenta nuevamente después de 15 minutos.'
 });
 
-
 app.use(express.json());
 app.use(cors());
 app.use('/api', apiLimiter, routes);
-
-
-import http from 'http';
-http.createServer((req, res) => {
-    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-    res.end();
-}).listen(httpPort, () => {
-    console.log(`Servidor HTTP redirigiendo al puerto HTTPS ${httpsPort}`);
-});
-
-// Configurar HTTPS
-const httpsOptions = {
-    cert: fs.readFileSync('/etc/letsencrypt/archive/jasai.site/fullchain1.pem'),
-    key: fs.readFileSync('/etc/letsencrypt/archive/jasai.site/privkey1.pem')
-};
-
-
-https.createServer(httpsOptions, app).listen(httpsPort, () => {
-    console.log(`Servidor HTTPS corriendo en el puerto ${httpsPort}`);
-});
-
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
