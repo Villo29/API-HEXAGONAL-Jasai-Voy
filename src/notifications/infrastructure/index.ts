10adc83bf615c3354024d5bf75ed21847758c75d
David Ruiz <jesuruga@gmail.com>
2024-11-30 22:35:01 -0600

    Agregar soporte para redirección HTTP a HTTPS y configurar servidores HTTPS en las infraestructuras de notificaciones, pagos y usuarios

--- src/notifications/infrastructure/index.ts
+++ src/notifications/infrastructure/index.ts
@@ -10,8 +10,8 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || '';
-// const httpPort = 80; // Puerto HTTP para redirección
-// const httpsPort = process.env.PORTU || 443; // Puerto HTTPS
+const httpPort = 80; // Puerto HTTP para redirección
+const httpsPort = process.env.PORTU || 443; // Puerto HTTPS
 
 if (!process.env.MERCADOPAGO_ACCESS_TOKEN) {
     console.error('ERROR: MERCADO_PAGO_ACCESS_TOKEN no está configurado.');
@@ -29,24 +29,24 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', apiLimiter, routes);
 
-// import http from 'http';
-// http.createServer((req, res) => {
-//     res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-//     res.end();
-// }).listen(httpPort, () => {
-//     console.log(`Servidor HTTP redirigiendo al puerto HTTPS ${httpsPort}`);
-// });
+import http from 'http';
+http.createServer((req, res) => {
+    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+    res.end();
+}).listen(httpPort, () => {
+    console.log(`Servidor HTTP redirigiendo al puerto HTTPS ${httpsPort}`);
+});
 
-// // Configurar HTTPS
-// const httpsOptions = {
-//     cert: fs.readFileSync('/etc/letsencrypt/live/jasai.site/fullchain.pem'),
-//     key: fs.readFileSync('/etc/letsencrypt/live/jasai.site/privkey.pem')
-// };
+// Configurar HTTPS
+const httpsOptions = {
+    cert: fs.readFileSync('/etc/letsencrypt/archive/jasai.site/fullchain1.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/archive/jasai.site/privkey1.pem')
+};
 
 
-// https.createServer(httpsOptions, app).listen(httpsPort, () => {
-//     console.log(`Servidor HTTPS corriendo en el puerto ${httpsPort}`);
-// });
+https.createServer(httpsOptions, app).listen(httpsPort, () => {
+    console.log(`Servidor HTTPS corriendo en el puerto ${httpsPort}`);
+});
 
 
 app.use('/', (req, res) => {
